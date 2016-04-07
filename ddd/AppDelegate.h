9633f52e1da37769bf93427f9d74492a7615d9ec
mac <mac@macdemacbook-pro-3.local>
2016-04-07 18:33:38 +0800

    Initial Commit

---
+++ ddd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ddd
+//
+//  Created by mac on 16/4/7.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
